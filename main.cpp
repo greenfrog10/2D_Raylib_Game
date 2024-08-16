5553bd07358f1accbd75c495467e30392c7d6114
greenfrog10 <100206121+greenfrog10@users.noreply.github.com>
2024-08-16 15:00:40 +0200

    Made the boxes movable by the player
    Will need to make the boxes collide beetween them in the future.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@ public:
 class Player {
 public:
     Vector2 pos;
-    int size;
     int speed;
     int y_velocity = 0;
     bool jumping;
@@ -19,13 +18,23 @@ public:
     bool fall = false;
     bool is_jumping = false;
     bool block_jump = false;
+    bool left = false;
+    bool right = false;
     int y_before_jump = 0;
     int max_jump_height = 250;
     Rectangle Rect;
     Texture2D sprite;
     void Update_Rect()
     {
-       Rect = {pos.x,pos.y,60,60};
+       Rect = {pos.x,pos.y,80,80};
+    }
+    void Respawn(Vector2 position)
+    {
+        pos = position;
+    }
+    void Show()
+    {
+        DrawTextureV(sprite,pos,WHITE);
     }
 };
 
@@ -48,17 +57,17 @@ public:
     bool is_on_floor = false;
     bool fall = true;
     bool show = true;
-    Rectangle Rect = {0, 0, 60, 60};   
-    void Fall(Rectangle groundRect,bool &is_on_floor, bool &fall, Vector2 &pos) 
+    int rect_width;
+    int rect_height;
+    Rectangle Rect = {0, 0, rect_width, rect_height};   
+    void Fall(Rectangle groundRect) 
     {
-        if (is_on_floor == false) fall = true;
-        if (is_on_floor == true) fall = false;
-        if (fall) pos.y += 9;
-        if (CheckCollisionRecs(Rect,groundRect)) is_on_floor = true;
-        else 
+        if (CheckCollisionRecs(Rect,groundRect)) fall = false;
+        else
         {
             fall = true;
         }
+        if (fall) pos.y += 9;
     }
     void CheckBulletCollision(Rectangle bulletRect, bool &shot_fired, bool &gun_follow_player) 
     {
@@ -73,9 +82,17 @@ public:
     {
         if (show)  DrawTextureV(sprite, pos, WHITE);
     }
-    void Update_Rect(int width,int height)
+    void Update_Rect()
     {
-        Rect = {pos.x, pos.y, width, height};
+        Rect = {pos.x, pos.y, rect_width, rect_height};
+    }
+    void Check_if_pushed(bool right,bool left,Rectangle p_rect)
+    {
+        if (CheckCollisionRecs(Rect,p_rect))
+        {
+            if (right) pos.x += 10;
+            if (left) pos.x -= 10;
+        }
     }
 };
 void Change_mouse_to_Target(Texture2D sprite,Vector2 &pos)
@@ -93,7 +110,6 @@ int main() {
     Vector2 center{window.width / 2, window.height / 2};
     
     Player player;
-    player.size = 60;
     player.speed = 10;
     SetTargetFPS(60);
     player.pos = center;
@@ -106,17 +122,30 @@ int main() {
     Object box;  
     box.pos.x = 1200;
     box.pos.y = 0;
+    box.rect_width = 80;
+    box.rect_height = 80;
     box.sprite = LoadTexture("sprites/box.png");
     
     Object box2; 
     box2.pos.x = 1300;
     box2.pos.y = 0;
+    box2.rect_width = 80;
+    box2.rect_height = 80;
     box2.sprite = box.sprite;
     
     Object box3;
     box3.pos.x = 1000;
     box3.pos.y = 0;
+    box3.rect_width = 80;
+    box3.rect_height = 80;
     box3.sprite = box.sprite;
+    
+    Object big_box;
+    big_box.pos.x = 700;
+    big_box.pos.y = 0;
+    big_box.rect_width = 160;
+    big_box.rect_height = 80;
+    big_box.sprite = LoadTexture("sprites/big_box.png");
     
     Object gun;
     gun.pos = player.pos;
@@ -139,24 +168,41 @@ int main() {
     // Main game loop
     while (!WindowShouldClose()) {
         // Check Key inputs and move player
-        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) player.pos.x += player.speed;
-        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) player.pos.x -= player.speed;
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
+        {
+            player.right = true;
+            player.left = false;
+        }
+        else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
+        {
+            player.left = true;
+            player.right = false;
+        }
+        else
+        {
+            player.left = false;
+            player.right = false;
+        }
         if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
-            if (!player.block_jump) {
+            if (!player.block_jump) 
+            {
                 if (player.is_on_floor) player.is_jumping = true;
                 player.block_jump = true;
                 player.is_on_floor = false;
                 player.y_before_jump = player.pos.y;
-            }
+            }         
         }
-        
+        // move player 
+        if (player.right) player.pos.x += player.speed;
+        if (player.left) player.pos.x -= player.speed;
         // Update_Rects
         player.Update_Rect();
         bullet.Update_Rect(50,50);
         Rectangle clickRect = {(float)click_x - 20, (float)click_y - 20, 20, 20};
-        box.Update_Rect(60,60);   
-        box2.Update_Rect(60,60);
-        box3.Update_Rect(60,60);
+        box.Update_Rect();   
+        box2.Update_Rect();
+        box3.Update_Rect();
+        big_box.Update_Rect();
         // Check Borders for player
         if (CheckCollisionRecs(player.Rect, ground)) player.is_on_floor = true;
         else player.is_on_floor = false;
@@ -175,9 +221,14 @@ int main() {
             player.block_jump = true;
         }
         if (player.fall) player.pos.y += 10;
-        box.Fall(ground,box.is_on_floor, box.fall, box.pos);
-        box2.Fall(ground,box2.is_on_floor, box2.fall, box2.pos);
-        box3.Fall(ground,box3.is_on_floor, box3.fall, box3.pos);
+        box.Fall(ground);
+        box2.Fall(ground);
+        box3.Fall(ground);
+        big_box.Fall(ground);
+        box.Check_if_pushed(player.right,player.left,player.Rect);
+        box2.Check_if_pushed(player.right,player.left,player.Rect);
+        box3.Check_if_pushed(player.right,player.left,player.Rect);
+        big_box.Check_if_pushed(player.right,player.left,player.Rect);
         Change_mouse_to_Target(target.sprite,target.pos);
         if (gun_follow_player) 
         {
@@ -206,10 +257,11 @@ int main() {
         }       
         // Draw The Objects on the screeen
         DrawRectangleRec(ground,PINK);
-        DrawTextureV(player.sprite, player.pos, WHITE);
         box.Show();
         box2.Show();
         box3.Show();
+        big_box.Show();
+        player.Show();
         Change_mouse_to_Target(target.sprite,target.pos);
         if (gun_facing_right)
         {
@@ -250,6 +302,7 @@ int main() {
             box.CheckBulletCollision(bullet.Rect, shot_fired, gun_follow_player);
             box2.CheckBulletCollision(bullet.Rect, shot_fired, gun_follow_player);
             box3.CheckBulletCollision(bullet.Rect, shot_fired, gun_follow_player);
+            big_box.CheckBulletCollision(bullet.Rect, shot_fired, gun_follow_player);
         }
         
         EndDrawing();
