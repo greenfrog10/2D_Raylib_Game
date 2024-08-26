409c9a5bd9d90f2f30cecaee9f5ecc485ca0f1e0
greenfrog10 <100206121+greenfrog10@users.noreply.github.com>
2024-08-26 19:36:01 +0200

    Updated the level3 made 4 blocks going at player direction to make the player learn to jump

--- levels/level3.cpp
+++ levels/level3.cpp
@@ -5,253 +5,276 @@
 #include "../floating_object/floating_object.h"
 #include "../object/object.h"
 #include "../change_mouse_to_target/change_mouse_to_target.h"
+
 bool run_level3() 
 {
     bool reload_level = false;
-    bool autorise_exit = false;
+    bool autorise_exit = true;
     bool run = true;
-    int box_max_speed = 15;
-    Window window;
-    window.width = 1600;
-    window.height = 900;
-    InitWindow(window.width, window.height, "level 3");
-    Vector2 center{window.width / 2, window.height / 2};
+    char* text = "Quick jump these boxes can kill you !";
+    while (run)
+    {
+        reload_level = false;
+        Window window;
+        window.width = 1600;
+        window.height = 900;
+        InitWindow(window.width, window.height, "level 3");
+        Vector2 center{window.width / 2, window.height / 2};
 
-    Player player;
-    player.speed = 15;
-    SetTargetFPS(60);
-    player.pos.x = 0;
-    player.pos.y = 500;
-    player.sprite = LoadTexture("sprites/player.png");
+        Player player;
+        player.speed = 15;
+        SetTargetFPS(60);
+        player.pos.x = 0;
+        player.pos.y = 500;
+        player.sprite = LoadTexture("sprites/player.png");
 
-    Floating_Object target;
-    target.pos = player.pos;
-    target.sprite = LoadTexture("sprites/target.png");
+        Floating_Object target;
+        target.pos = player.pos;
+        target.sprite = LoadTexture("sprites/target.png");
 
-    Object gun;
-    gun.pos = player.pos;
-    Texture2D gun_left = LoadTexture("sprites/gun_left.png");
-    Texture2D gun_right = LoadTexture("sprites/gun_right.png");
-    gun.sprite = gun_right;
-    bool gun_facing_left = false;
-    bool gun_facing_right = true;
- 
-    Object box;
-    box.pos.x = center.x;
-    box.pos.y = 720;
-    box.sprite = LoadTexture("sprites/metal_box.png");
-    box.rect_width = 121;
-    box.rect_height = 115;
-    box.Move_left();
-    
-    Object box2;
-    box2.pos.x = center.x + 500;
-    box2.pos.y = 720;
-    box2.sprite = box.sprite;
-    box2.rect_width = box.rect_width;
-    box2.rect_height = box.rect_height;
-    box2.Move_left();
-    
-    Floating_Object bullet;
-    int click_x = GetMouseX();
-    int click_y = GetMouseY();
-    int bullet_x_destination = 0;
-    int bullet_y_destination = 0;
-    bool gun_follow_player = true;
-    Rectangle bulletRect = {bullet.pos.x - 20, bullet.pos.y - 20, 60, 60};
-    bool shot_fired = false;
-    bool gun_disabled = false;
-    Rectangle ground = {0, 800, 1600, 100};
+        Object gun;
+        gun.pos = player.pos;
+        Texture2D gun_left = LoadTexture("sprites/gun_left.png");
+        Texture2D gun_right = LoadTexture("sprites/gun_right.png");
+        gun.sprite = gun_right;
+        bool gun_facing_left = false;
+        bool gun_facing_right = true;
 
-    // Main game loop
-    while (run) {
-        HideCursor();
+        Object box;
+        box.speed = 10;
+        box.pos.x = center.x;
+        box.pos.y = 720;
+        box.sprite = LoadTexture("sprites/dmg_box.png");
+        box.rect_width = 121;
+        box.rect_height = 115;
+        box.Move_left();
 
-        // Check Key inputs and move player
-        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) 
+        Object box2;
+        box2.pos.x = box.pos.x + 500;
+        box2.pos.y = 720;
+        box2.sprite = box.sprite;
+        box2.rect_width = box.rect_width;
+        box2.rect_height = box.rect_height;
+        box2.Move_left();
+        
+        Object box3;
+        box3.pos.x = box2.pos.x + 500;
+        box3.pos.y = 720;
+        box3.sprite = box.sprite;
+        box3.rect_width = box.rect_width;
+        box3.rect_height = box.rect_height;
+        box3.Move_left();
+        
+        Object box4;
+        box4.pos.x = box3.pos.x + 500;
+        box4.pos.y = 720;
+        box4.sprite = box.sprite;
+        box4.rect_width = box.rect_width;
+        box4.rect_height = box.rect_height;
+        box4.Move_left();
+
+        Floating_Object bullet;
+        int click_x = GetMouseX();
+        int click_y = GetMouseY();
+        int bullet_x_destination = 0;
+        int bullet_y_destination = 0;
+        bool gun_follow_player = true;
+        Rectangle bulletRect = {bullet.pos.x - 20, bullet.pos.y - 20, 60, 60};
+        bool shot_fired = false;
+        bool gun_disabled = false;
+        Rectangle ground = {0, 800, 3000, 100};
+        // Main game loop
+        while (!reload_level)
         {
-                player.right = true;
+            HideCursor();
+
+            // Check Key inputs and move player
+            if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) 
+            {
+                    player.right = true;
+                    player.left = false;
+            } 
+            else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) 
+            {
+                player.left = true;
+                player.right = false;
+            } 
+            else 
+            {
                 player.left = false;
-         } 
-         else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) 
-         {
-            player.left = true;
-            player.right = false;
-         } 
-         else 
-         {
-            player.left = false;
-            player.right = false;
-         }
+                player.right = false;
+            }
 
-         if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) 
-         {
-           if (!player.block_jump) 
-           {
-                if (player.is_on_floor) player.is_jumping = true;
+            if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) 
+            {
+                if (!player.block_jump) 
+                {
+                    if (player.is_on_floor) player.is_jumping = true;
+                    player.block_jump = true;
+                    player.is_on_floor = false;
+                    player.y_before_jump = player.pos.y;
+                }
+            }
+            if (IsKeyDown(KEY_R))
+            {
+                reload_level = true;
+            }
+
+            if (WindowShouldClose())
+            {
+                run = false;
+                break;
+                return false;
+            }
+
+            // Move player 
+            if (player.right) player.pos.x += player.speed;
+            if (player.left) player.pos.x -= player.speed;
+
+            // Update Rectangles
+            box2.speed = box.speed;
+            box3.speed = box.speed;
+            box4.speed = box.speed;
+            box.Fall(ground);
+            box.Update_Position();
+            box.Update_Rect();
+            
+            box2.Fall(ground);
+            box2.Update_Position();
+            box2.Update_Rect();
+            
+            box3.Fall(ground);
+            box3.Update_Position();
+            box3.Update_Rect();
+            
+            box4.Fall(ground);
+            box4.Update_Position();
+            box4.Update_Rect();
+            
+            player.Update_Rect();
+            
+            bullet.Update_Rect(50, 50);
+            
+            Rectangle clickRect = {(float)click_x - 20, (float)click_y - 20, 20, 20};
+            // check if box touched player 
+            if (box.Check_collision_with_rect(player.Rect) || box2.Check_collision_with_rect(player.Rect)) player.show = false;
+            if (box3.Check_collision_with_rect(player.Rect)) player.show = false;
+
+            // Check Borders and Ground for player
+            if (CheckCollisionRecs(player.Rect, ground)) player.is_on_floor = true;
+            else player.is_on_floor = false;
+
+            if (player.pos.x <= 10) player.pos.x = 10;
+            if (player.pos.x >= window.width - 100)
+            {
+                if (autorise_exit)
+                {
+                    run = false;
+                    break;
+                }              
+                player.pos.x = window.width - 100;
+            }
+            if (player.pos.x >= window.width - 100 && autorise_exit) run = false;
+            if (!player.is_on_floor && !player.is_jumping) player.fall = true;
+            if (player.is_jumping) player.pos.y -= 30;
+            if (player.y_before_jump - player.pos.y >= player.max_jump_height) player.is_jumping = false;
+    
+            if (player.is_on_floor) {
+                player.fall = false;
+                player.block_jump = false;
+            } else {
+                player.fall = true;
                 player.block_jump = true;
-                player.is_on_floor = false;
-                player.y_before_jump = player.pos.y;
-           }
-          }
-         if (IsKeyDown(KEY_R))
-         {
-             reload_level = true;
-         }
+            }
 
-        if (WindowShouldClose())
-        {
-            break;
-            return false;
-        }
+            if (player.fall) player.pos.y += 10;
+            if (gun_follow_player) {
+                bullet.pos = gun.pos;
+                if (gun_facing_right) {
+                    bullet.pos.x += 100;
+                }
+                if (gun_facing_left) {
+                    bullet.pos.x -= 20;
+                }
+            }
+            // Draw the window
+            BeginDrawing();
+            ClearBackground(PURPLE);
 
-        // Move player 
-        if (player.right) player.pos.x += player.speed;
-        if (player.left) player.pos.x -= player.speed;
+            if (target.pos.x < center.x) {
+                gun_facing_right = false;
+                gun_facing_left = true;
+            }
+            if (target.pos.x > center.x) {
+                gun_facing_right = true;
+                gun_facing_left = false;
+            }
 
-        // Update Rectangles
-        box2.speed = box.speed;
-        box.Fall(ground);
-        box.Update_Position();
-        box.Update_Rect();
-        
-        box2.Fall(ground);
-        box2.Update_Position();
-        box2.Update_Rect();
-        
-        player.Update_Rect();
-        
-        bullet.Update_Rect(50, 50);
-        
-        Rectangle clickRect = {(float)click_x - 20, (float)click_y - 20, 20, 20};
-        // check if box touched player 
-        if (box.Check_collision_with_rect(player.Rect) || box2.Check_collision_with_rect(player.Rect)) player.show = false;
-
-        // Check Borders and Ground for player
-        if (CheckCollisionRecs(player.Rect, ground)) player.is_on_floor = true;
-        else player.is_on_floor = false;
-
-        if (player.pos.x <= 10) player.pos.x = 10;
-        if (player.pos.x >= window.width - 100) player.pos.x = window.width - 100;
-        if (player.pos.x >= window.width - 100 && autorise_exit) run = false;
-        if (!player.is_on_floor && !player.is_jumping) player.fall = true;
-        if (player.is_jumping) player.pos.y -= 25;
-        if (player.y_before_jump - player.pos.y >= player.max_jump_height) player.is_jumping = false;
-
-        if (player.is_on_floor) {
-            player.fall = false;
-            player.block_jump = false;
-        } else {
-            player.fall = true;
-            player.block_jump = true;
-        }
-
-        if (player.fall) player.pos.y += 10;
-        if (box.pos.x <= 0)
-        {
-            box.Move_right();
-            if (box.speed < box_max_speed)
+            // Draw The Objects on the screen
+            DrawRectangleRec(ground, PINK);
+            player.Show();
+            box.Show();
+            box2.Show();
+            box3.Show();
+            box4.Show();
+            DrawText(text,0,0,80,WHITE);
+            if (!player.show)
             {
-                box.speed += 0.5;
+                gun_disabled = true;
+                text = "Press R to retry";
             }
-        }
-        if (box2.pos.x <= 0) box2.Move_right();
-        if (box.pos.x >= window.width - 100)
-        {
-            box.Move_left();
-            if (box.speed < box_max_speed)
-            {
-                box.speed += 0.5;
-            }
-        }
-        if (box2.pos.x >= window.width - 100) box2.Move_left();
-        if (gun_follow_player) {
-            bullet.pos = gun.pos;
-            if (gun_facing_right) {
-                bullet.pos.x += 100;
-            }
-            if (gun_facing_left) {
-                bullet.pos.x -= 20;
-            }
-        }
+            if (!gun_disabled) {
+                Change_mouse_to_Target(target.sprite, target.pos);
 
-        // Draw the window
-        BeginDrawing();
-        ClearBackground(PURPLE);
+                if (gun_facing_right) {
+                    gun.pos.x = player.pos.x + 80;
+                    gun.pos.y = player.pos.y;
+                    gun.sprite = gun_right;
+                }
+                if (gun_facing_left) {
+                    gun.pos.x = player.pos.x - 80;
+                    gun.pos.y = player.pos.y;
+                    gun.sprite = gun_left;
+                }
 
-        if (target.pos.x < center.x) {
-            gun_facing_right = false;
-            gun_facing_left = true;
-        }
-        if (target.pos.x > center.x) {
-            gun_facing_right = true;
-            gun_facing_left = false;
-        }
+                DrawTextureV(gun.sprite, gun.pos, WHITE);
 
-        // Draw The Objects on the screen
-        DrawRectangleRec(ground, PINK);
-        player.Show();
-        box.Show();
-        box2.Show();
-        if (!player.show)
-        {
-            gun_disabled = true;
-            DrawText("Press R to retry",0,0,100,WHITE);
-        }
-        if (reload_level)
-        {
-            gun_disabled = true;
-            CloseWindow();
-            run_level3();
-            reload_level = false;
-        }
-        if (!gun_disabled) {
-            Change_mouse_to_Target(target.sprite, target.pos);
+                // Check if left mouse button is released, shoot the bullet
+                if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                    if (!shot_fired) {
+                        click_x = GetMouseX();
+                        click_y = GetMouseY();
+                        shot_fired = true;
+                    }
+                }
 
-            if (gun_facing_right) {
-                gun.pos.x = player.pos.x + 80;
-                gun.pos.y = player.pos.y;
-                gun.sprite = gun_right;
-            }
-            if (gun_facing_left) {
-                gun.pos.x = player.pos.x - 80;
-                gun.pos.y = player.pos.y;
-                gun.sprite = gun_left;
-            }
-
-            DrawTextureV(gun.sprite, gun.pos, WHITE);
-
-            // Check if left mouse button is released, shoot the bullet
-            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-                if (!shot_fired) {
-                    click_x = GetMouseX();
-                    click_y = GetMouseY();
-                    shot_fired = true;
+                if (shot_fired) {
+                    gun_follow_player = false;
+                    DrawCircle(bullet.pos.x, bullet.pos.y, 20, YELLOW);
+                    bullet_x_destination = (click_x - bullet.pos.x) / 15;
+                    bullet_y_destination = (click_y - bullet.pos.y) / 15;
+                    if (CheckCollisionRecs(bullet.Rect, clickRect)) {
+                        shot_fired = false;
+                        gun_follow_player = true;
+                    } else {
+                        bullet.pos.x += bullet_x_destination;
+                        bullet.pos.y += bullet_y_destination;
+                    }
                 }
             }
 
-            if (shot_fired) {
-                gun_follow_player = false;
-                DrawCircle(bullet.pos.x, bullet.pos.y, 20, YELLOW);
-                bullet_x_destination = (click_x - bullet.pos.x) / 15;
-                bullet_y_destination = (click_y - bullet.pos.y) / 15;
-                if (CheckCollisionRecs(bullet.Rect, clickRect)) {
-                    shot_fired = false;
-                    gun_follow_player = true;
-                } else {
-                    bullet.pos.x += bullet_x_destination;
-                    bullet.pos.y += bullet_y_destination;
-                }
-            }
+            EndDrawing();
         }
 
-        EndDrawing();
+        UnloadTexture(player.sprite);
+        UnloadTexture(target.sprite);
+        UnloadTexture(gun_left);
+        UnloadTexture(gun_right);
+        UnloadTexture(box.sprite);
+
+        CloseWindow();
+        text = "Quick jump these boxes can kill you !";
     }
-    UnloadTexture(player.sprite);
-    UnloadTexture(target.sprite);
-    UnloadTexture(gun_left);
-    UnloadTexture(gun_right);
 
-    CloseWindow();
     return true;
 }
