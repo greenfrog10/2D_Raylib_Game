8bfe7f337c7d162f0c11e3feaa0a87b82c024394
greenfrog10 <100206121+greenfrog10@users.noreply.github.com>
2024-08-28 20:12:07 +0200

    Made the movement system in the player file
    So we can directly import the Functions to move the player

--- player/player.cpp
+++ player/player.cpp
@@ -3,11 +3,60 @@ void Player::Update_Rect()
 {
     Rect = {pos.x, pos.y, 80, 80};
 }
-void Player::Respawn(Vector2 position) 
-{
-    pos = position;
-}
 void Player::Show()
 {
-    DrawTextureV(sprite, pos, WHITE);
+    if (show)
+    {
+        DrawTextureV(sprite, pos, WHITE);
+    }
+}
+void Player::Left()
+{
+    left = true;
+    right = false;
+}
+void Player::Right()
+{
+    right = true;
+    left = false;
+}
+void Player::Stop()
+{
+    right = false;
+    left = false;
+}
+void Player::Update_Position(int width, Rectangle ground)
+{
+    if (right) pos.x += speed;
+    if (left) pos.x -= speed;   
+    if (pos.x >= width - 100) pos.x = width - 100;
+    if(CheckCollisionRecs(ground,Rect)) is_on_floor = true;
+    else
+    {
+    is_on_floor = false;
+    }
+    if (!is_on_floor && !is_jumping) fall = true;
+    if (is_jumping) pos.y -= 30;
+    if (y_before_jump - pos.y >= max_jump_height) is_jumping = false;
+    if (is_on_floor) 
+    {
+        fall = false;
+        block_jump = false;
+     } 
+     else 
+     {
+        fall = true;
+        block_jump = true;
+     }
+     if (fall) pos.y += 10;
+}
+void Player::Jump()
+{
+    if (!block_jump) 
+    {
+        if (is_on_floor) is_jumping = true;
+        block_jump = true;
+        is_on_floor = false;
+        y_before_jump = pos.y;
+    }
 }
