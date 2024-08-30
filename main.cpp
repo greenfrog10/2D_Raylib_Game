dc8e60870d89a760279ae0da54fff7f557a5dbbd
greenfrog10 <100206121+greenfrog10@users.noreply.github.com>
2024-08-30 22:52:01 +0200

    Compiled the code

--- main.cpp
+++ main.cpp
@@ -2,14 +2,10 @@
 #include "levels/level1.h" 
 #include "levels/level2.h"
 #include "levels/level3.h"
+#include "levels/level4.h"
 #include <iostream>
 int main() 
 {
-    if(run_level1())
-    {
-        if(run_level2())
-        {
-            run_level3();
-        }
-    }
+    // if(run_level1()) if(run_level2()) if(run_level3())  run_level4();
+    run_level4();
 }
