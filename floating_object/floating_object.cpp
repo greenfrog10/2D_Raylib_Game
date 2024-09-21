2aa566e8fae1174c6f1a7a602c06ebc7a9efee9a
greenfrog10 <100206121+greenfrog10@users.noreply.github.com>
2024-09-21 16:08:45 +0200

    Delete floating_object directory

--- floating_object/floating_object.cpp
+++
@@ -1,5 +0,0 @@
-#include "floating_object.h"
-void Floating_Object::Update_Rect(int width, int height) 
-{
-    Rect = {pos.x - 20, pos.y - 20, width, height};
-}
--- floating_object/floating_object.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "raylib.h"
-class Floating_Object {
-public:
-    Vector2 pos;
-    Texture2D sprite;
-    Rectangle Rect;
-    void Update_Rect(int width, int height);
-};
