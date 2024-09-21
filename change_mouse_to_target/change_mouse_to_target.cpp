59bde4f98b83ff21a31bca51ade6fcd348118fad
greenfrog10 <100206121+greenfrog10@users.noreply.github.com>
2024-09-21 16:08:38 +0200

    Delete change_mouse_to_target directory

--- change_mouse_to_target/change_mouse_to_target.cpp
+++
@@ -1,6 +0,0 @@
-#include "change_mouse_to_target.h"
-
-void Change_mouse_to_Target(Texture2D sprite, Vector2 &pos) {
-    pos = GetMousePosition();
-    DrawTextureV(sprite, pos, WHITE);
-}
--- change_mouse_to_target/change_mouse_to_target.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "raylib.h"
-
-void Change_mouse_to_Target(Texture2D sprite, Vector2 &pos);
-
